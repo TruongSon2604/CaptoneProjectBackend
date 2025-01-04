795777a7943acf4d7a7696417d8918dca8e3cae7
TruongSon2604 <truongngocson26042003@gmail.com>
2025-01-04 12:15:59 +0700

    Add longtitude latitude

--- database/migrations/2024_12_28_025656_create_orders_table.php
+++ database/migrations/2024_12_28_025656_create_orders_table.php
@@ -18,6 +18,8 @@ return new class extends Migration {
             $table->decimal('discount_amount', 10, 2)->default(0); // Số tiền giảm giá
             $table->decimal('final_amount', 10, 2); // Tổng tiền sau giảm giá
             // $table->unsignedBigInteger('shipper_id')->nullable();
+            $table->decimal('latitude', 10, 6);  // Vĩ độ của khách hàng
+            $table->decimal('longitude', 10, 6);  // Kinh độ của khách hàng
             $table->enum('status', ['pending', 'in_progress', 'completed', 'canceled'])->default('pending');
             $table->decimal('total_amount', 10, 2);  // Tổng số tiền
             $table->decimal('shipping_fee', 10, 2);  // Phí vận chuyển
